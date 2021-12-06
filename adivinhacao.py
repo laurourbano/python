8734ca526f9d4f64136d1e18c463656e1dda5535
laurourbano <74082012+laurourbano@users.noreply.github.com>
2021-12-06 00:09:54 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,14 +3,27 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-if (numero_secreto == chute):
-    print("Você acertou!")
-else:
-    print("Você errou!")
+    acertou = chute == numero_secreto
+    chutou_acima = chute > numero_secreto
+    chutou_abaixo = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+        break
+    else:
+        if(chutou_acima):
+            print("Você errou, o número secreto é menor!")
+        elif(chutou_abaixo):
+            print("Você errou, o número secreto é maior!")
+    rodada = rodada + 1
 
 print("Fim do jogo")

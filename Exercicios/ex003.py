a750db27383c5393dd312b6b370793489c902c28
laurourbano <74082012+laurourbano@users.noreply.github.com>
2021-12-29 00:19:40 -0300

    ex.python

---
+++ Exercicios/ex003.py
@@ -0,0 +1,3 @@
+numero_um = input('digite o primeiro número: ')
+numero_dois = input('digite o segundo número: ')
+print('a soma entre o número 'f'{numero_um} e o número 'f'{numero_dois} é ', int(numero_um) + int(numero_dois))

b38d235ce77172ad2d35884a97cdf93561a5fd76
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-02-03 18:56:41 -0300

    novas atualizações ex

--- Exercicios/ex006a.py
+++ Exercicios/ex006a.py
@@ -6,4 +6,4 @@ n = int(input('Digite um número: '))
 d = n * 2
 t = n * 3
 r = n ** (1/2)
-print('O dobro de 'f'{n} ''vale 'f'{d}.\n''O triplo de 'f'{n} ' 'vale 'f'{t}.\n''A raiz quadrada de 'f'{n} ''vale 'f'{r:.2f}')
+print('\nO dobro de 'f'{n} ''vale 'f'{d}.\n''O triplo de 'f'{n} ' 'vale 'f'{t}.\n''A raiz quadrada de 'f'{n} ''vale 'f'{r:.2f}\n\n' 'Obrigado por utilizar este programa!\n')

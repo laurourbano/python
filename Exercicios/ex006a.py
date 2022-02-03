1777cd932775395fb819eb86001ec4fb96c51783
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-02-03 18:50:55 -0300

    Confirmar em 'main'

---
+++ Exercicios/ex006a.py
@@ -0,0 +1,9 @@
+from re import T
+from tkinter import N
+
+
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O dobro de 'f'{n} ''vale 'f'{d}.\n''O triplo de 'f'{n} ' 'vale 'f'{t}.\n''A raiz quadrada de 'f'{n} ''vale 'f'{r:.2f}')
